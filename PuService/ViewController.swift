04bde78f513853f9dd416dfcbfca6561b2afb1c1
akapan <akapan.r@hitachi-tstv.com>
2019-01-24 11:46:19 +0700

    Initial Commit

---
+++ PuService/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PuService
+//
+//  Created by TST-APP-02 on 24/1/2562 BE.
+//  Copyright © 2562 Nardanong.s. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
