2a8a1fd1ff2bb7109d921c4a99d87845b8c1f955
akapan <akapan.r@hitachi-tstv.com>
2019-01-25 10:07:09 +0700

    Find URL Add User  Success

---
+++ PuService/MyConstant.swift
@@ -0,0 +1,21 @@
+//
+//  MyConstant.swift
+//  PuService
+//
+//  Created by TST-APP-02 on 25/1/2562 BE.
+//  Copyright © 2562 Nardanong.s. All rights reserved.
+//
+
+import Foundation
+
+class MyConstant  {
+    
+    func findURLaddUser(name:String,user:String,password:String) -> String {
+        let result = "https://www.androidthai.in.th/hit/addUserPu.php?isAdd=true&Name=\(name)&User=\(user)&Password=\(password)"
+        return result
+    }
+    
+    
+}//MyConstant Class
+
+
--- PuService/RegisterViewControler.swift
+++ PuService/RegisterViewControler.swift
@@ -30,6 +30,24 @@ class RegisterViewControler: UIViewController {
         // Do any additional setup after loading the view.
     }//Main Method
     
+    func uploadToServer(name:String,user:String,password:String) -> Void {
+        
+        let myConstant = MyConstant()
+        let urlPHP:String = myConstant.findURLaddUser(name: name, user: user, password: password)
+        print("URLPHP ==> \(urlPHP)")
+        
+    }
+    
+    
+    func showAlert(title: String, message: String) -> Void {
+        
+        let objAlert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
+        objAlert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: { (action) in
+            objAlert.dismiss(animated: true, completion: nil)
+        }))
+        self.present(objAlert, animated: true, completion: nil)
+    }
+    
     @IBAction func uploadButton(_ sender: UIBarButtonItem) {
         
 //        Get value from textfield
@@ -44,13 +62,13 @@ class RegisterViewControler: UIViewController {
         
 //        Check state
         if (name!.count == 0) || (user!.count == 0) || (pasword!.count == 0) {
-//            Have space
+//        Have space
             print("Have Space")
+            showAlert(title: "Have Space", message: "Please fill every blank")
         }else{
             print("No Space")
+            uploadToServer(name: name!, user: user!, password: pasword!)
         }
-        
-        
         
     }//Upload Buuton
     
