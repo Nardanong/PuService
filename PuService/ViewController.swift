4c596f3286ab3696b8f5f023f853c39384bdf7ce
akapan <akapan.r@hitachi-tstv.com>
2019-01-25 14:37:16 +0700

    Authentication password success

--- PuService/ViewController.swift
+++ PuService/ViewController.swift
@@ -42,9 +42,37 @@ class ViewController: UIViewController {
                 return
             }
             
+//            Read Json From API
             do {
                 let JSONResponse = try JSONSerialization.jsonObject(with: dataResponse, options: [])
                 print("JsonResponse ==> \(JSONResponse)")
+                
+//             change json to Array
+                guard let jsonArray = JSONResponse as? [[String:Any]]else {
+                    return
+                }
+                
+                print("jsonArray ==> \(jsonArray)")
+                
+                guard let jsonDictionary:Dictionary = jsonArray[0] else {
+                    return
+                }
+                print("jsonDictionary ==> \(jsonDictionary)")
+//                print("truePassword ==> \(jsonDictionary["Password"]!)")
+                
+                let truePassword:String = jsonDictionary["Password"]! as! String
+                print("truePassword ==>\(truePassword)")
+                
+                if password == truePassword {
+                    DispatchQueue.main.async {
+                        self.performSegue(withIdentifier: "GoService", sender: self)
+                    }
+                }else{
+                    DispatchQueue.main.async {
+                        self.myAlert(title: "Password false", message: "Please try again password")
+                    }
+                }
+                
                 
             }catch let myError {
                 print(myError)
